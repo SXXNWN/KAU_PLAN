793660857cd537321b2df195d882fa111d6e20f2
rexmx5 <69381113+rexmx5@users.noreply.github.com>
2025-12-16 18:59:33 +0900

    지출데이터 정의

--- app/src/main/java/com/example/kau_plan/data/Expense.kt
+++ app/src/main/java/com/example/kau_plan/data/Expense.kt
@@ -1,11 +1,12 @@
 package com.example.kau_plan.data
 
 data class Expense(
-    val title: String,
-    val category: String,
-    val payer: String,
-    val date: String,
-    val amount: Int
+    val title: String = "",
+    val category: String = "",
+    val payer: String = "",
+    val date: String = "",
+    val amount: Int = 0,
+    val id: String = ""
 )
 
 val dummyExpenses = listOf(
